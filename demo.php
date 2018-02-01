cb166adf0f655664d6607096b1a14b03bb503df7
Miss-you <snowfly1993@gmail.com>
2018-02-01 15:22:49 +0800

    easy demo

---
+++ demo.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+$dateTime = gmdate("I d F Y H:i:s");
+$SecretId = 'AKIDgz33go7zufbgrt6azbakwbx7tx0jampv84kz';
+$SecretKey = 'lCIC0ZQhtcI5u36Lojuh2bnOBqaKy6r5FF4Qc1';
+$srcStr = "date: ".$dateTime."\n"."source: "."yousali";
+$Authen = 'hmac id="'.$SecretId.'", algorithm="hmac-sha1", headers="date source", signature="';
+$signStr = base64_encode(hash_hmac('sha1', $srcStr, $SecretKey, true));
+//echo $signStr;
+$Authen = $Authen.$signStr."\"";
+echo $Authen;
+#echo '</br>';
+
+$url = 'http://service-3mm3bc6g-1251762227.ap-guangzhou.apigateway.myqcloud.com/release/yousa';
+$headers = array( 
+	'Host:service-3mm3bc6g-1251762227.ap-guangzhou.apigateway.myqcloud.com',
+	'Accept:text/html, */*; q=0.01',
+	'Source: yousali',
+	'Date: '.$dateTime,
+	'Authorization: '.$Authen,
+	'X-Requested-With: XMLHttpRequest',
+	'Accept-Encoding: gzip, deflate, sdch'
+);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL,$url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+$data = curl_exec($ch); 
+
+if (curl_errno($ch)) { 
+	print "Error: " . curl_error($ch); 
+} else { 
+	// Show me the result 
+	var_dump($data); 
+	curl_close($ch); 
+} 
+
+?>
